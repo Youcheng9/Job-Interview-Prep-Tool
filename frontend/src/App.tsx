dca3eb58c76f117b11ccdb59b47951b0c80ba974
F Y <yfreeman102804@gmail.com>
2026-04-01 15:32:46 -0700

    Added route for authentication page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./app/page";
 import InterviewPage from "./app/interview/page";
 import HistoryPage from "./app/history/page";
+import AuthPage from "./app/auth/page";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/interview" element={<InterviewPage />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
